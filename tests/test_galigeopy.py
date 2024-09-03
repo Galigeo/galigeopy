5a1da8834b13be8173df86684223574eb1d1d931
JPTGGO <jpierrat@galigeo.com>
2024-09-03 14:28:57 +0200

    tests

--- tests/test_galigeopy.py
+++ tests/test_galigeopy.py
@@ -6,12 +6,7 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from galigeopy.galigeopy import check
-from tests.org.test_org import TestOrg
 
 class TestGaligeopy(unittest.TestCase):
     def test_check(self):
         self.assertTrue(check())
-
-
-if __name__ == '__main__':
-    unittest.main()
