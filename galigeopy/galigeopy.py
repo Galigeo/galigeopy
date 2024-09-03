aa8eb19f5acb85a86fad8a344d6de9671a16a1de
JPTGGO <jpierrat@galigeo.com>
2024-09-03 14:24:25 +0200

    new version 0.0.3

--- galigeopy/galigeopy.py
+++ galigeopy/galigeopy.py
@@ -1,8 +1,9 @@
 def about():
-    print("This is a simple Python package for geocoding and reverse geocoding.")
-    print("It uses the Google Maps API.")
-    print("It was created by Galileo Galilei.")
-    print("It is distributed under the MIT License.")
+    print("This is a simple Python package to interract with Galigeo's products")
+    print("Author: Jules Pierrat")
+    print("License: Apache License 2.0")
+    print("Code source: https://github.com/JPTGGO/galigeopy")
+    print("Documentation: https://galigeopy.readthedocs.io/en/latest/")
 
 def check():
     return True
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import setuptools
 
-VERSION = "0.0.2"  # PEP-440
+VERSION = "0.0.3"  # PEP-440
 
 NAME = "galigeopy"
 
