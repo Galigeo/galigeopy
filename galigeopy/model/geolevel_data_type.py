ebeb7077e729614ce9a1067a4d1e0eb38d7e0222
JPTGGO <jpierrat@galigeo.com>
2024-09-17 11:19:33 +0200

    Geolevel data type

--- galigeopy/model/geolevel_data_type.py
+++ galigeopy/model/geolevel_data_type.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import geopandas as gpd
+import json
 
 from sqlalchemy import text
 
@@ -13,11 +14,11 @@ class GeolevelDataType:
             geolevel_id:int,
             org:'Org' # type: ignore
     ):
-        self.geolevel_data_type_id = custdata_type_id
-        self.name = name
-        self.properties = properties
-        self.geolevel_id = geolevel_id
-        self.org = org
+        self._geolevel_data_type_id = custdata_type_id
+        self._name = name
+        self._properties = properties
+        self._geolevel_id = geolevel_id
+        self._org = org
 
     # Getters
     @property
@@ -30,3 +31,41 @@ class GeolevelDataType:
     def geolevel_id(self): return self._geolevel_id
     @property
     def org(self): return self._org
+
+    # Public Methods
+    def getGeolevel(self):
+        return self._org.getGeolevelById(self._geolevel_id)
+    
+    def getDataset(self)->pd.DataFrame:
+        query = text(f"SELECT * FROM ggo_geolevel_data WHERE geolevel_data_type_id = {self.geolevel_data_type_id}")
+        return pd.read_sql(query, self._org.engine)
+    
+    def getGeoDataset(self)->gpd.GeoDataFrame:
+        geolevel = self.getGeolevel()
+        query = text(f"""
+            SELECT
+                g.*,
+                geo.{geolevel.geom_field} AS geometry
+            FROM ggo_geolevel_data g
+            JOIN {geolevel.table_name} AS geo ON geo.{geolevel.geounit_code} = g.geounit_code
+            WHERE
+                geolevel_data_type_id = {self.geolevel_data_type_id}
+        """)
+        return gpd.read_postgis(query, self._org.engine, geom_col='geometry')
+    
+    def add_to_model(self):
+        # add to database
+        query = f"""
+            INSERT INTO ggo_geoleveldata_type (
+                name,
+                properties,
+                geolevel_id
+            ) VALUES (
+                '{self.name.replace("'", "''")}',
+                '{json.dumps(self.properties).replace("'", "''")}',
+                {self.geolevel_id}
+            ) RETURNING geolevel_data_type_id
+        """
+        geolevel_data_type_id = self._org.query(query)[0][0]
+        # return
+        return geolevel_data_type_id
