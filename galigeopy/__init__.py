1ee551bd58bed4ab96eea47439fa3473098721af
JPTGGO <jpierrat@galigeo.com>
2024-10-04 07:20:50 +0200

    Restruct lib

--- galigeopy/__init__.py
+++ galigeopy/__init__.py
@@ -2,20 +2,14 @@
 from galigeopy.galigeopy import about, check
 
 # Org
-from galigeopy.org.org import Org
+from galigeopy.org import *
 
 # Model
-from galigeopy.model.network import Network
-from galigeopy.model.poi import Poi
-from galigeopy.model.zone_type import ZoneType
-from galigeopy.model.zone import Zone
-from galigeopy.model.zone_geounit import ZoneGeounit
-from galigeopy.model.geolevel import Geolevel
-from galigeopy.model.geolevel_data_type import GeolevelDataType
+from galigeopy.model import *
 # from galigeopy.model.geolevel_data import GeolevelData
 
 # Engine
-from galigeopy.engine.osrm_engine import OsrmEngine
+from galigeopy.engine import *
 
 # Utils
-from galigeopy.utils.spherical_calculus import haversine_distance
+from galigeopy.utils import *
